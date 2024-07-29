b6c8b170e4a54309d8f359b700608e25665d570a
brian <505072907@qq.com>
2024-07-29 14:38:35 +0800

    update

---
+++ src/views/areaManage/index.spec.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import AreaComponent from './index'
+describe('Router', () => {
+    test('AreaComponent Settings', () => {
+        expect(AreaComponent).not.toBe(undefined);
+      });
+})
